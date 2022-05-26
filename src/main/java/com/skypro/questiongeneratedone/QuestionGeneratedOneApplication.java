d8725889e4ffa40a01f2a19fb60346732936a3c4
dgobozov <saniamit@mail.ru>
2022-05-26 18:47:25 +0300

    Initial commit

---
+++ src/main/java/com/skypro/questiongeneratedone/QuestionGeneratedOneApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.questiongeneratedone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGeneratedOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionGeneratedOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/questiongeneratedone/QuestionGeneratedOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.questiongeneratedone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGeneratedOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
